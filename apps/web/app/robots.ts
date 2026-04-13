def19bc507b257b6914823f5719976fb2c925cc4
Judo <judobehera0708@gmail.com>
2026-04-13 23:14:03 +0530

    seo temp robots.ts and sitemap

---
+++ apps/web/app/robots.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: [
+          '/',
+          '/auth/signin',
+          '/privacy',
+          '/terms',
+        ],
+        disallow: [
+          '/api/',
+          '/auth/onboarding',
+          '/auth/error',
+          '/auth/age-gate',
+          '/profile',
+          '/create',
+          '/notifications',
+          '/companion/',
+          '/admin/',
+          '/admin-generate',
+          '/_next/',
+        ],
+      },
+    ],
+    sitemap: 'https://blushbite.co/sitemap.xml',
+    host: 'https://blushbite.co',
+  }
+}
